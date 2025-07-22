cc557e783ec36161e3ebc6a76fda9666501c1b4e
Favslight <samuelebiloma208@gmail.com>
2025-07-22 15:02:12 +0100

    great

--- Backend/app.js
+++ Backend/app.js
@@ -83,7 +83,7 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT =process.env.PORT || 5000;
+const PORT =process.env.PORT || 4000;
 require('./cron/investmentProcessor');
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
